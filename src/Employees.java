5a4d1a549d9ffdef8cfa32049f124ca6837afae8
Simon Blaha <s.blaha@live.at>
2019-05-14 14:13:21 +0200

    push basic exercise 3

---
+++ src/Employees.java
@@ -0,0 +1,11 @@
+public class Employees {
+
+    String name;
+    int age;
+    int hourlyRates;
+
+    public String toString() {
+        String employee = name + " " + age + " " + hourlyRates;
+        return employee;
+    }
+}

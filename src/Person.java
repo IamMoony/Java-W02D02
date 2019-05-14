b381838c88ddf438a9d977bd2a8600a76a80899b
Simon Blaha <s.blaha@live.at>
2019-05-14 09:29:12 +0200

    creating classes, rename main

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Person {
+    int id;
+    String name;
+    int age;
+
+    public Person(int id, String name, int age) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+
 }

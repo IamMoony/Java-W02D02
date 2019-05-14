f7ce17841a92c8ed30245eef2d90af0ec43af3a5
tjennyyy <jenny_tang@gmx.net>
2019-05-14 10:53:07 +0200

    adding

--- src/Teacher.java
+++ src/Teacher.java
@@ -2,7 +2,7 @@ public class Teacher extends Person {
 
     private String major;
 
-    public Teacher(int id, String name, int age, int average) {
+    public Teacher(int id, String name, int age, int major) {
         super(id, name, age);
 
     }

e45780ac6550739ee1b258ee394632e3b6bdf439
tjennyyy <jenny_tang@gmx.net>
2019-05-14 13:53:59 +0200

    finish rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,6 +1,17 @@
 public class Rectangle extends Shape {
 
-    public String Rectangle(){
+    double widht;
+    double length;
 
+    public Rectangle(double width, double length){
+        this.widht = widht;
+        this.length = length;
     }
+
+    @Override
+    public double area (){
+        return widht+length;
+
+     }
+
 }
